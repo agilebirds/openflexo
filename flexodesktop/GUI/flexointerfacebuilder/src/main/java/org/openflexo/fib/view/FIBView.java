294d8189515d2a57c9c011e355cf517798ad136d
Sylvain Guérin <sylvain@openflexo.org>
2012-04-02 13:54:44 +0200

    MEDIUM / Prevent NPE

--- flexodesktop/GUI/flexointerfacebuilder/src/main/java/org/openflexo/fib/view/FIBView.java
+++ flexodesktop/GUI/flexointerfacebuilder/src/main/java/org/openflexo/fib/view/FIBView.java
@@ -279,16 +279,20 @@ public abstract class FIBView<M extends FIBComponent, J extends JComponent> impl
 
 	public void notifyDynamicModelChanged() {
 		// System.out.println("notifyDynamicModelChanged()");
-		Iterator<FIBComponent> it = getComponent().getMayAltersIterator();
-		while (it.hasNext()) {
-			FIBComponent c = it.next();
-			logger.fine("Because dynamic model change, now update " + c);
-			FIBView view = getController().viewForComponent(c);
-			if (view != null) {
-				view.updateDataObject(getDataObject());
-			} else {
-				logger.warning("Unexpected null view when retrieving view for " + c);
+		if (getComponent() != null) {
+			Iterator<FIBComponent> it = getComponent().getMayAltersIterator();
+			while (it.hasNext()) {
+				FIBComponent c = it.next();
+				logger.fine("Because dynamic model change, now update " + c);
+				FIBView view = getController().viewForComponent(c);
+				if (view != null) {
+					view.updateDataObject(getDataObject());
+				} else {
+					logger.warning("Unexpected null view when retrieving view for " + c);
+				}
 			}
+		} else {
+			logger.warning("Unexpected null component");
 		}
 	}
 
