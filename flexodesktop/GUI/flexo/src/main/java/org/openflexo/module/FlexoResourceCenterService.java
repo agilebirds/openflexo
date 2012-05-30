5a05738e851f77d986243f318b15b1fbcc83b454
gpolet <guillaume.polet@gmail.com>
2012-05-30 21:50:37 +0200

    LOW: Fixed dialog owner

--- flexodesktop/GUI/flexo/src/main/java/org/openflexo/module/FlexoResourceCenterService.java
+++ flexodesktop/GUI/flexo/src/main/java/org/openflexo/module/FlexoResourceCenterService.java
@@ -31,6 +31,7 @@ import org.openflexo.foundation.LocalResourceCenterImplementation;
 import org.openflexo.foundation.rm.FlexoProject;
 import org.openflexo.foundation.utils.ProjectExitingCancelledException;
 import org.openflexo.localization.FlexoLocalization;
+import org.openflexo.view.FlexoFrame;
 
 public class FlexoResourceCenterService {
 
@@ -79,7 +80,8 @@ public class FlexoResourceCenterService {
 					AskLocalResourceCenterDirectory data = new AskLocalResourceCenterDirectory();
 					data.setLocalResourceDirectory(FlexoProject.getResourceCenterFile());
 					FIBDialog<AskLocalResourceCenterDirectory> dialog = FIBDialog.instanciateAndShowDialog(
-							AskLocalResourceCenterDirectory.FIB_FILE, data, null, true, FlexoLocalization.getMainLocalizer());
+							AskLocalResourceCenterDirectory.FIB_FILE, data, FlexoFrame.getActiveFrame(), true,
+							FlexoLocalization.getMainLocalizer());
 					switch (dialog.getStatus()) {
 					case VALIDATED:
 						if (data.getLocalResourceDirectory() != null) {
