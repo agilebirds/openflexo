80f75948904fc159d4891b5438fa0aeaafb3efdf
Sylvain Guérin <sylvain@openflexo.org>
2012-09-06 13:05:33 +0200

    MEDIUM / Utils: getMostSpecializedClass() method

--- flexodesktop/model/flexoutils/src/main/java/org/openflexo/antar/binding/TypeUtils.java
+++ flexodesktop/model/flexoutils/src/main/java/org/openflexo/antar/binding/TypeUtils.java
@@ -687,6 +687,37 @@ public class TypeUtils {
 		return object;
 	}
 
+	public static Class getMostSpecializedClass(Collection<Class> someClasses) {
+
+		if (someClasses.size() == 0) {
+			return null;
+		}
+		if (someClasses.size() == 1) {
+			return someClasses.iterator().next();
+		}
+		Class[] array = someClasses.toArray(new Class[someClasses.size()]);
+
+		for (int i = 0; i < someClasses.size(); i++) {
+			for (int j = i + 1; j < someClasses.size(); j++) {
+				Class c1 = array[i];
+				Class c2 = array[j];
+				if (c1.isAssignableFrom(c2)) {
+					someClasses.remove(c1);
+					return getMostSpecializedClass(someClasses);
+				}
+				if (c2.isAssignableFrom(c1)) {
+					someClasses.remove(c2);
+					return getMostSpecializedClass(someClasses);
+				}
+			}
+		}
+
+		// No parent were found, take first item
+		logger.warning("Undefined specializing criteria between " + someClasses);
+		return someClasses.iterator().next();
+
+	}
+
 	// TESTS
 
 	public static interface ShouldFail {
