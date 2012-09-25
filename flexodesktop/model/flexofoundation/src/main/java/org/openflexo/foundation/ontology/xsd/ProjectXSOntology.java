129ff845bd518775d359643a5f97244f2c9b69c1
Luka Le Roux <luka@openflexo.org>
2012-09-25 18:02:14 +0200

    Support XSD: Fixes
    Note: Without the model slot, you need to modify:
    FlexoProject.createProjectOntology()
    Go there for more details

--- flexodesktop/model/flexofoundation/src/main/java/org/openflexo/foundation/ontology/xsd/ProjectXSOntology.java
+++ flexodesktop/model/flexofoundation/src/main/java/org/openflexo/foundation/ontology/xsd/ProjectXSOntology.java
@@ -1,11 +1,15 @@
 package org.openflexo.foundation.ontology.xsd;
 
 import java.io.File;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import org.openflexo.foundation.Inspectors;
 import org.openflexo.foundation.ontology.FlexoOntology;
 import org.openflexo.foundation.ontology.OntologyLibrary;
+import org.openflexo.foundation.ontology.ProjectOWLOntology;
 import org.openflexo.foundation.ontology.ProjectOntology;
+import org.openflexo.foundation.ontology.ProjectOntologyLibrary;
 import org.openflexo.foundation.ontology.owl.OWLClass;
 import org.openflexo.foundation.ontology.owl.OWLOntology.OntologyNotFoundException;
 import org.openflexo.foundation.ontology.owl.OWLProperty;
@@ -13,14 +17,23 @@ import org.openflexo.foundation.ontology.owl.OntologyRestrictionClass;
 import org.openflexo.foundation.ontology.owl.OntologyRestrictionClass.RestrictionType;
 import org.openflexo.foundation.rm.DuplicateResourceException;
 import org.openflexo.foundation.rm.FlexoProject;
+import org.openflexo.foundation.rm.FlexoProjectOntologyResource;
 import org.openflexo.foundation.rm.FlexoResource;
 import org.openflexo.foundation.rm.FlexoXMLModelResource;
+import org.openflexo.foundation.rm.InvalidFileNameException;
+import org.openflexo.foundation.rm.ProjectRestructuration;
 import org.openflexo.foundation.rm.SaveResourceException;
+import org.openflexo.foundation.utils.FlexoProjectFile;
 
 import com.hp.hpl.jena.ontology.OntModel;
+import com.hp.hpl.jena.ontology.OntModelSpec;
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
 
 public class ProjectXSOntology extends XSOntology implements ProjectOntology {
 
+	protected static final Logger logger = Logger.getLogger(ProjectXSOntology.class.getPackage().getName());
+	
 	private FlexoXMLModelResource modelResource;
 	private FlexoProject project;
 
@@ -91,4 +104,57 @@ public class ProjectXSOntology extends XSOntology implements ProjectOntology {
 		return "XSD_project_ontology";
 	}
 
+	public static ProjectXSOntology createNewProjectOntology(FlexoProject project) {
+		if (logger.isLoggable(Level.FINE)) {
+			logger.fine("createNewProjectOntology(), project=" + project);
+		}
+		logger.info("-------------> Create XSD ontology for " + project.getProjectName());
+		File owlFile = ProjectRestructuration.getExpectedProjectOntologyFile(project, project.getProjectName());
+		FlexoProjectFile ontologyFile = new FlexoProjectFile(owlFile, project);
+
+		ProjectXSOntology newProjectOntology = createProjectOntology(project.getURI(), owlFile, project.getProjectOntologyLibrary());
+		project.getProjectOntologyLibrary().registerOntology(newProjectOntology);
+
+		FlexoXMLModelResource ontologyRes;
+		try {
+			ontologyRes = new FlexoXMLModelResource(project, newProjectOntology, ontologyFile);
+		} catch (InvalidFileNameException e) {
+			e.printStackTrace();
+			if (logger.isLoggable(Level.SEVERE)) {
+				logger.severe("This should not happen: invalid file " + ontologyFile);
+			}
+			return null;
+		} catch (DuplicateResourceException e) {
+			e.printStackTrace();
+			if (logger.isLoggable(Level.SEVERE)) {
+				logger.severe("This should not happen: DuplicateResourceException for " + ontologyFile);
+			}
+			return null;
+		}
+		try {
+			project.registerResource(ontologyRes);
+		} catch (Exception e1) {
+			if (logger.isLoggable(Level.WARNING)) {
+				logger.warning("Exception raised: " + e1.getClass().getName() + ". See console for details.");
+			}
+			e1.printStackTrace();
+		}
+
+		try {
+			ontologyRes.saveResourceData();
+		} catch (Exception e1) {
+			if (logger.isLoggable(Level.WARNING)) {
+				logger.warning("Exception raised: " + e1.getClass().getName() + ". See console for details.");
+			}
+			e1.printStackTrace();
+		}
+
+		return newProjectOntology;
+	}
+	
+	private static ProjectXSOntology createProjectOntology(String anURI, File xsdFile, ProjectOntologyLibrary ontologyLibrary) {
+		ProjectXSOntology returned = new ProjectXSOntology(anURI, xsdFile, ontologyLibrary);
+
+		return returned;
+	}
 }
