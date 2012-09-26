ff175fa15676a7e0dc8654e74a95f2c6b4d37fc0
Luka Le Roux <luka@openflexo.org>
2012-09-26 15:49:08 +0200

    Model Slots: initial commit

---
+++ flexodesktop/model/flexofoundation/src/main/java/org/openflexo/foundation/modelslot/OWLModelSlot.java
@@ -0,0 +1,19 @@
+package org.openflexo.foundation.modelslot;
+
+import org.openflexo.foundation.ontology.ProjectOWLOntology;
+import org.openflexo.foundation.ontology.owl.OWLOntology;
+
+public class OWLModelSlot extends AbstractModelSlot <OWLOntology> {
+
+	@Override
+	public TechnologicalSpace getTechnologicalSpace() {
+		return TechnologicalSpace.OWL;
+	}
+	
+	@Override
+	public ProjectOWLOntology createEmptyModel() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ flexodesktop/model/flexofoundation/src/main/java/org/openflexo/foundation/modelslot/TechnologicalSpace.java
@@ -0,0 +1,34 @@
+package org.openflexo.foundation.modelslot;
+
+/**
+ * <p>Lists the supported technological spaces.
+ * </br>Also provides (eventually) meta informations about them.
+ * 
+ * @author Luka Le Roux
+ *
+ */
+public enum TechnologicalSpace {
+
+	XSD {
+		@Override
+		protected XSModelSlot newSlot() {
+			return new XSModelSlot();
+		}
+	},
+	
+	OWL {
+		@Override
+		protected OWLModelSlot newSlot() {
+			return new OWLModelSlot();
+		}
+	};
+	
+	/**
+	 * <p>Don't use this method.
+	 * </br>To create a {@link ModelSlot} use the {@link ModelSlotParameters} class and its {@link #ModelSlotParameters.create()} method instead.
+	 * 
+	 * @return a new {@link ModelSlot}
+	 */
+	protected abstract ModelSlot<?> newSlot();
+	
+}
