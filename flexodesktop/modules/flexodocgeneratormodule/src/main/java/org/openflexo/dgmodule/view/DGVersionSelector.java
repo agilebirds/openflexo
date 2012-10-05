9537b38e91b518d319517d8e4bc91167fe1faefe
gpolet <guillaume.polet@gmail.com>
2012-10-05 19:27:53 +0200

    LOW: Removed useless lines

--- flexodesktop/modules/flexodocgeneratormodule/src/main/java/org/openflexo/dgmodule/view/DGVersionSelector.java
+++ flexodesktop/modules/flexodocgeneratormodule/src/main/java/org/openflexo/dgmodule/view/DGVersionSelector.java
@@ -89,7 +89,6 @@ public class DGVersionSelector extends AbstractBrowserSelector<AbstractCGFileVer
 		@Override
 		public Dimension getDefaultSize() {
 			Dimension returned = _browserView.getDefaultSize();
-			returned.width = returned.width;
 			returned.height = returned.height - 100;
 			return returned;
 		}
