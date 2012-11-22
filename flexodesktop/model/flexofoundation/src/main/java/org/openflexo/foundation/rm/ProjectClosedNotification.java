d9e8a5655d60994feca6ab84cd531af313d0beb0
gpolet <guillaume.polet@gmail.com>
2012-11-22 01:24:26 +0100

    LOW: Extracted notification constant. This is referenced by the FlexoController

--- flexodesktop/model/flexofoundation/src/main/java/org/openflexo/foundation/rm/ProjectClosedNotification.java
+++ flexodesktop/model/flexofoundation/src/main/java/org/openflexo/foundation/rm/ProjectClosedNotification.java
@@ -4,8 +4,10 @@ import org.openflexo.foundation.DataModification;
 
 public class ProjectClosedNotification extends DataModification {
 
+	public static final String CLOSE = "close";
+
 	public ProjectClosedNotification(FlexoProject project) {
-		super("close", project, project);
+		super(CLOSE, project, project);
 	}
 
 }
