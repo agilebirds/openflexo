f064830f4ff9b8778d07d382755bfeb3494edfbb
Sylvain Guérin <sylvain@openflexo.org>
2013-01-21 10:21:02 +0100

    MEDIUM / Commented references to FIBAbstractEditor in src/main/java directories

--- flexodesktop/modules/flexoviewpointmodeler/src/main/java/org/openflexo/vpm/widget/FIBDiagramPaletteBrowser.java
+++ flexodesktop/modules/flexoviewpointmodeler/src/main/java/org/openflexo/vpm/widget/FIBDiagramPaletteBrowser.java
@@ -19,24 +19,12 @@
  */
 package org.openflexo.vpm.widget;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import org.openflexo.TestApplicationContext;
-import org.openflexo.fib.controller.FIBController;
-import org.openflexo.fib.editor.FIBAbstractEditor;
-import org.openflexo.fib.model.FIBComponent;
-import org.openflexo.foundation.rm.DiagramPaletteResource;
-import org.openflexo.foundation.rm.ViewPointResource;
 import org.openflexo.foundation.view.diagram.viewpoint.DiagramPalette;
-import org.openflexo.foundation.viewpoint.ViewPointLibrary;
-import org.openflexo.logging.FlexoLoggingManager;
 import org.openflexo.toolbox.FileResource;
 import org.openflexo.view.FIBBrowserView;
 import org.openflexo.view.controller.FlexoController;
-import org.openflexo.view.controller.FlexoFIBController;
 
 /**
  * Browser allowing to browse through example diagram<br>
@@ -57,7 +45,7 @@ public class FIBDiagramPaletteBrowser extends FIBBrowserView<DiagramPalette> {
 	// Please uncomment this for a live test
 	// Never commit this uncommented since it will not compile on continuous build
 	// To have icon, you need to choose "Test interface" in the editor (otherwise, flexo controller is not insanciated in EDIT mode)
-	public static void main(String[] args) {
+	/*public static void main(String[] args) {
 
 		try {
 			FlexoLoggingManager.initialize(-1, true, null, Level.INFO, null);
@@ -97,6 +85,6 @@ public class FIBDiagramPaletteBrowser extends FIBBrowserView<DiagramPalette> {
 		};
 		editor.launch();
 
-	}
+	}*/
 
 }
