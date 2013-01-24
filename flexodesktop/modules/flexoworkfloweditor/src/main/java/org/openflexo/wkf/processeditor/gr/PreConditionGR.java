bbd4c9095fb35dd4bbae08ad1edefca5f417371a
gpolet <guillaume.polet@gmail.com>
2013-01-24 17:30:35 +0100

    LOW: Precondition are now transparents

--- flexodesktop/modules/flexoworkfloweditor/src/main/java/org/openflexo/wkf/processeditor/gr/PreConditionGR.java
+++ flexodesktop/modules/flexoworkfloweditor/src/main/java/org/openflexo/wkf/processeditor/gr/PreConditionGR.java
@@ -19,7 +19,6 @@
  */
 package org.openflexo.wkf.processeditor.gr;
 
-import java.awt.Color;
 import java.awt.geom.AffineTransform;
 import java.util.Observable;
 import java.util.logging.Logger;
@@ -72,9 +71,10 @@ public class PreConditionGR extends AbstractNodeGR<FlexoPreCondition> implements
 		// setY(getFlexoPreCondition().getPosY());
 		setWidth(PRECONDITION_SIZE);
 		setHeight(PRECONDITION_SIZE);
-		foreground = ForegroundStyle.makeStyle(Color.BLACK);
-		foreground.setLineWidth(0.6);
-		background = BackgroundStyle.makeColoredBackground(Color.WHITE);
+		foreground = ForegroundStyle.makeNone();
+		background = BackgroundStyle.makeEmptyBackground();
+		background.setTransparencyLevel(0.0f);
+		background.setUseTransparency(true);
 		setHasText(false);
 		setForeground(foreground);
 		setBackground(background);
