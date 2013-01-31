d61a3db9b780df85887ce127540a0acc3ebeb7c4
gpolet <guillaume.polet@gmail.com>
2013-01-31 11:36:38 +0100

    LOW: Simple wrapping panel than can gray out a panel and disable all key bindings and mouse events

---
+++ flexodesktop/GUI/flexographicutils/src/main/java/org/openflexo/swing/GlassPaneWrapper.java
@@ -0,0 +1,41 @@
+package org.openflexo.swing;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+
+import javax.swing.JComponent;
+import javax.swing.JPanel;
+import javax.swing.KeyStroke;
+
+public class GlassPaneWrapper extends JPanel {
+
+	public GlassPaneWrapper(JComponent wrappedComponent) {
+		super(new BorderLayout());
+		super.addImpl(wrappedComponent, BorderLayout.CENTER, 0);
+	}
+
+	@Override
+	public boolean contains(int x, int y) {
+		return false;
+	}
+
+	@Override
+	protected boolean processKeyBinding(KeyStroke ks, KeyEvent e, int condition, boolean pressed) {
+		return false;
+	}
+
+	@Override
+	protected void addImpl(Component comp, Object constraints, int index) {
+
+	}
+
+	@Override
+	public void paint(Graphics g) {
+		super.paint(g);
+		g.setColor(new Color(240, 240, 240, 128));
+		g.fillRect(0, 0, getWidth(), getHeight());
+	}
+}
