2e9ad873b23891481a793133908afd0522969796
gpolet <guillaume.polet@gmail.com>
2013-02-04 23:27:40 +0100

    LOW: Fixed potential NPE

--- flexodesktop/GUI/flexo/src/main/java/org/openflexo/view/FlexoMainPane.java
+++ flexodesktop/GUI/flexo/src/main/java/org/openflexo/view/FlexoMainPane.java
@@ -131,7 +131,8 @@ public class FlexoMainPane extends JPanel implements PropertyChangeListener {
 			@Override
 			public Icon getTabHeaderIcon(Location tab) {
 				ImageIcon iconForObject = getController().iconForObject(tab.getObject());
-				if (tab.getObject() != null && tab.getObject().getProject() != getController().getControllerModel().getCurrentProject()) {
+				if (iconForObject != null && tab.getObject() != null
+						&& tab.getObject().getProject() != getController().getControllerModel().getCurrentProject()) {
 					iconForObject = IconFactory.getImageIcon(iconForObject, IconLibrary.LOCKED);
 				}
 				return iconForObject;
