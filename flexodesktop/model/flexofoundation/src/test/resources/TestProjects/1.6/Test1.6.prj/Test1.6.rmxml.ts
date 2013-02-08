9096b10a80a633d947fad26939ee9536a23087a8
Sylvain Guérin <sylvain@openflexo.org>
2013-02-08 22:11:50 +0100

    LOW / Test prj

--- flexodesktop/model/flexofoundation/src/test/resources/TestProjects/1.6/Test1.6.prj/Test1.6.rmxml.ts
+++ flexodesktop/model/flexofoundation/src/test/resources/TestProjects/1.6/Test1.6.prj/Test1.6.rmxml.ts
@@ -1,16 +1,16 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <Project id="FLX_42" name="Test1.6" userID="FLX" flexoID="42" dontEscapeLatex="false">
-  <FlexoStorageResource id="FLX_RM.Test1.6" className="org.openflexo.foundation.rm.FlexoRMResource" lastKnownMemoryUpdate="HH:mm:ss dd/MM/yyyy SSS,21:06:15 24/01/2013 881" file="Test1.6.rmxml" lastWrittenOnDisk="HH:mm:ss dd/MM/yyyy SSS,21:06:16 24/01/2013 105" userID="FLX" />
-  <TOCResource id="FLX_TOC_RESOURCE.Test1.6" userID="FLX" lastKnownMemoryUpdate="HH:mm:ss dd/MM/yyyy SSS,21:06:15 24/01/2013 862" file="Documentation/Test1.6.toc" lastWrittenOnDisk="HH:mm:ss dd/MM/yyyy SSS,21:06:15 24/01/2013 927" />
-  <FlexoWebServerFileResource id="FLX_WEBSERVER.001-Dialog.jpg" userID="FLX" file="ImportedImages/001-Dialog.jpg" lastWrittenOnDisk="HH:mm:ss dd/MM/yyyy SSS,21:06:15 24/01/2013 871" />
-  <FlexoWebServerFileResource id="FLX_WEBSERVER.002-Information.jpg" userID="FLX" file="ImportedImages/002-Information.jpg" lastWrittenOnDisk="HH:mm:ss dd/MM/yyyy SSS,21:06:15 24/01/2013 872" />
-  <FlexoWebServerFileResource id="FLX_WEBSERVER.003-FillForm.jpg" userID="FLX" file="ImportedImages/003-FillForm.jpg" lastWrittenOnDisk="HH:mm:ss dd/MM/yyyy SSS,21:06:15 24/01/2013 873" />
-  <FlexoWebServerFileResource id="FLX_WEBSERVER.004-InformationComputer.jpg" userID="FLX" file="ImportedImages/004-InformationComputer.jpg" lastWrittenOnDisk="HH:mm:ss dd/MM/yyyy SSS,21:06:15 24/01/2013 874" />
-  <FlexoWebServerFileResource id="FLX_WEBSERVER.005-AssemblyLine.jpg" userID="FLX" file="ImportedImages/005-AssemblyLine.jpg" lastWrittenOnDisk="HH:mm:ss dd/MM/yyyy SSS,21:06:15 24/01/2013 875" />
-  <FlexoWebServerFileResource id="FLX_WEBSERVER.006-FlexoServerVideo.jpg" userID="FLX" file="ImportedImages/006-FlexoServerVideo.jpg" lastWrittenOnDisk="HH:mm:ss dd/MM/yyyy SSS,21:06:15 24/01/2013 876" />
-  <FlexoWebServerFileResource id="FLX_WEBSERVER.007-Audio.jpg" userID="FLX" file="ImportedImages/007-Audio.jpg" lastWrittenOnDisk="HH:mm:ss dd/MM/yyyy SSS,21:06:15 24/01/2013 877" />
-  <FlexoWebServerFileResource id="FLX_WEBSERVER.agileMapping.jpg" userID="FLX" file="ImportedImages/agileMapping.jpg" lastWrittenOnDisk="HH:mm:ss dd/MM/yyyy SSS,21:06:15 24/01/2013 878" />
-  <FlexoWebServerFileResource id="FLX_WEBSERVER.objective-process.jpg" userID="FLX" file="ImportedImages/objective-process.jpg" lastWrittenOnDisk="HH:mm:ss dd/MM/yyyy SSS,21:06:15 24/01/2013 879" />
-  <FlexoWebServerFileResource id="FLX_WEBSERVER.organisationStructure.jpg" userID="FLX" file="ImportedImages/organisationStructure.jpg" lastWrittenOnDisk="HH:mm:ss dd/MM/yyyy SSS,21:06:15 24/01/2013 880" />
-  <FlexoWebServerFileResource id="FLX_WEBSERVER.process-mapping.jpg" userID="FLX" file="ImportedImages/process-mapping.jpg" lastWrittenOnDisk="HH:mm:ss dd/MM/yyyy SSS,21:06:15 24/01/2013 881" />
+  <FlexoStorageResource id="FLX_RM.Test1.6" className="org.openflexo.foundation.rm.FlexoRMResource" lastKnownMemoryUpdate="HH:mm:ss dd/MM/yyyy SSS,22:08:21 08/02/2013 991" file="Test1.6.rmxml" lastWrittenOnDisk="HH:mm:ss dd/MM/yyyy SSS,22:08:22 08/02/2013 218" userID="FLX" />
+  <TOCResource id="FLX_TOC_RESOURCE.Test1.6" userID="FLX" lastKnownMemoryUpdate="HH:mm:ss dd/MM/yyyy SSS,22:08:21 08/02/2013 848" file="Documentation/Test1.6.toc" lastWrittenOnDisk="HH:mm:ss dd/MM/yyyy SSS,22:08:21 08/02/2013 990" />
+  <FlexoWebServerFileResource id="FLX_WEBSERVER.001-Dialog.jpg" userID="FLX" file="ImportedImages/001-Dialog.jpg" lastWrittenOnDisk="HH:mm:ss dd/MM/yyyy SSS,22:08:21 08/02/2013 929" />
+  <FlexoWebServerFileResource id="FLX_WEBSERVER.002-Information.jpg" userID="FLX" file="ImportedImages/002-Information.jpg" lastWrittenOnDisk="HH:mm:ss dd/MM/yyyy SSS,22:08:21 08/02/2013 940" />
+  <FlexoWebServerFileResource id="FLX_WEBSERVER.003-FillForm.jpg" userID="FLX" file="ImportedImages/003-FillForm.jpg" lastWrittenOnDisk="HH:mm:ss dd/MM/yyyy SSS,22:08:21 08/02/2013 942" />
+  <FlexoWebServerFileResource id="FLX_WEBSERVER.004-InformationComputer.jpg" userID="FLX" file="ImportedImages/004-InformationComputer.jpg" lastWrittenOnDisk="HH:mm:ss dd/MM/yyyy SSS,22:08:21 08/02/2013 943" />
+  <FlexoWebServerFileResource id="FLX_WEBSERVER.005-AssemblyLine.jpg" userID="FLX" file="ImportedImages/005-AssemblyLine.jpg" lastWrittenOnDisk="HH:mm:ss dd/MM/yyyy SSS,22:08:21 08/02/2013 944" />
+  <FlexoWebServerFileResource id="FLX_WEBSERVER.006-FlexoServerVideo.jpg" userID="FLX" file="ImportedImages/006-FlexoServerVideo.jpg" lastWrittenOnDisk="HH:mm:ss dd/MM/yyyy SSS,22:08:21 08/02/2013 945" />
+  <FlexoWebServerFileResource id="FLX_WEBSERVER.007-Audio.jpg" userID="FLX" file="ImportedImages/007-Audio.jpg" lastWrittenOnDisk="HH:mm:ss dd/MM/yyyy SSS,22:08:21 08/02/2013 946" />
+  <FlexoWebServerFileResource id="FLX_WEBSERVER.agileMapping.jpg" userID="FLX" file="ImportedImages/agileMapping.jpg" lastWrittenOnDisk="HH:mm:ss dd/MM/yyyy SSS,22:08:21 08/02/2013 947" />
+  <FlexoWebServerFileResource id="FLX_WEBSERVER.objective-process.jpg" userID="FLX" file="ImportedImages/objective-process.jpg" lastWrittenOnDisk="HH:mm:ss dd/MM/yyyy SSS,22:08:21 08/02/2013 949" />
+  <FlexoWebServerFileResource id="FLX_WEBSERVER.organisationStructure.jpg" userID="FLX" file="ImportedImages/organisationStructure.jpg" lastWrittenOnDisk="HH:mm:ss dd/MM/yyyy SSS,22:08:21 08/02/2013 964" />
+  <FlexoWebServerFileResource id="FLX_WEBSERVER.process-mapping.jpg" userID="FLX" file="ImportedImages/process-mapping.jpg" lastWrittenOnDisk="HH:mm:ss dd/MM/yyyy SSS,22:08:21 08/02/2013 965" />
 </Project>
