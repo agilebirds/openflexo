9f056645d03565c1f7d82a029e749bf59fb8ef37
gpolet <guillaume.polet@gmail.com>
2013-04-09 15:41:09 +0200

    LOW: Fixed incorrect condition adding wrongly the lock icon

--- flexodesktop/GUI/flexo/src/main/java/org/openflexo/view/FlexoMainPane.java
+++ flexodesktop/GUI/flexo/src/main/java/org/openflexo/view/FlexoMainPane.java
@@ -132,7 +132,7 @@ public class FlexoMainPane extends JPanel implements PropertyChangeListener {
 			@Override
 			public Icon getTabHeaderIcon(Location tab) {
 				ImageIcon iconForObject = getController().iconForObject(tab.getObject());
-				if (iconForObject != null && tab.getObject() != null && tab.getObject() instanceof FlexoProjectObject && tab.isEditable()) {
+				if (iconForObject != null && tab.getObject() != null && tab.getObject() instanceof FlexoProjectObject && !tab.isEditable()) {
 					iconForObject = IconFactory.getImageIcon(iconForObject, IconLibrary.LOCKED);
 				}
 				return iconForObject;
