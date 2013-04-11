3faf09b1b62e376b3cae8fad7fc8f3ffec98d74c
Sylvain Guérin <sylvain@openflexo.org>
2013-04-11 11:25:32 +0200

    IMPORTANT / Fixed URI not set for VirtualModelInstance resource

--- flexodesktop/model/flexofoundation/src/main/java/org/openflexo/foundation/rm/VirtualModelInstanceResourceImpl.java
+++ flexodesktop/model/flexofoundation/src/main/java/org/openflexo/foundation/rm/VirtualModelInstanceResourceImpl.java
@@ -45,6 +45,7 @@ public abstract class VirtualModelInstanceResourceImpl<VMI extends VirtualModelI
 			returned.setProject(view.getProject());
 			returned.setName(name);
 			returned.setFile(xmlFile);
+			returned.setURI(view.getProject().getURI() + "/" + baseName);
 			returned.setVirtualModelResource(virtualModel.getResource());
 
 			returned.setServiceManager(view.getProject().getServiceManager());
@@ -71,6 +72,7 @@ public abstract class VirtualModelInstanceResourceImpl<VMI extends VirtualModelI
 			File xmlFile = new File(viewResource.getFile().getParentFile(), baseName + VirtualModelInstanceResource.VIRTUAL_MODEL_SUFFIX);
 			returned.setProject(viewResource.getProject());
 			returned.setName(baseName);
+			returned.setURI(viewResource.getProject().getURI() + "/" + baseName);
 			returned.setFile(xmlFile);
 			VirtualModelInstanceInfo vmiInfo = findVirtualModelInstanceInfo(xmlFile, "VirtualModelInstance");
 			if (vmiInfo == null) {
