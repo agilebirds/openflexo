40b0d1c28efac36050bd24bf2b11b0be83f40020
Vincent <vincent@openflexo.org>
2013-07-04 21:56:39 +0200

    MEDIUM / Separation of each FIB to configure Model Slot instance. Before we had only one FIB to configure virtual model slot instance and type safe model slot instance, while now there is one FIB for each of these plus one for the Free Model slot instance(no metamodel). This impact also the CreateVirtualModelInstanceInitializer which in in charge of FIB selection.

--- flexodesktop/modules/flexovieweditor/src/main/java/org/openflexo/ve/VECst.java
+++ flexodesktop/modules/flexovieweditor/src/main/java/org/openflexo/ve/VECst.java
@@ -51,7 +51,10 @@ public class VECst {
 	// View/VirtualModelInstance edition
 	public static File CREATE_VIEW_DIALOG_FIB = new FileResource("Fib/Dialog/CreateViewDialog.fib");
 	public static File CREATE_VIRTUAL_MODEL_INSTANCE_DIALOG_FIB = new FileResource("Fib/Dialog/CreateVirtualModelInstanceDialog.fib");
-	public static File CONFIGURE_MODEL_SLOT_INSTANCE_DIALOG_FIB = new FileResource("Fib/Dialog/ConfigureModelSlotInstanceDialog.fib");
+	public static File CONFIGURE_FREE_MODEL_SLOT_INSTANCE_DIALOG_FIB = new FileResource(
+			"Fib/Dialog/ConfigureFreeModelSlotInstanceDialog.fib");
+	public static File CONFIGURE_TYPESAFE_MODEL_SLOT_INSTANCE_DIALOG_FIB = new FileResource(
+			"Fib/Dialog/ConfigureTypeSafeModelSlotInstanceDialog.fib");
 
 	// Diagram edition
 	public static File CREATE_DIAGRAM_DIALOG_FIB = new FileResource("Fib/Dialog/CreateDiagramDialog.fib");
--- flexodesktop/modules/flexovieweditor/src/main/java/org/openflexo/ve/controller/action/CreateDiagramInitializer.java
+++ flexodesktop/modules/flexovieweditor/src/main/java/org/openflexo/ve/controller/action/CreateDiagramInitializer.java
@@ -19,6 +19,7 @@
  */
 package org.openflexo.ve.controller.action;
 
+import java.io.File;
 import java.util.EventObject;
 import java.util.logging.Logger;
 
@@ -32,9 +33,12 @@ import org.openflexo.foundation.action.FlexoActionInitializer;
 import org.openflexo.foundation.action.FlexoExceptionHandler;
 import org.openflexo.foundation.action.NotImplementedException;
 import org.openflexo.foundation.rm.DuplicateResourceException;
+import org.openflexo.foundation.technologyadapter.FreeModelSlot;
 import org.openflexo.foundation.technologyadapter.ModelSlot;
+import org.openflexo.foundation.technologyadapter.TypeAwareModelSlot;
 import org.openflexo.foundation.view.View;
 import org.openflexo.foundation.view.diagram.action.CreateDiagram;
+import org.openflexo.foundation.viewpoint.VirtualModelModelSlot;
 import org.openflexo.icon.VEIconLibrary;
 import org.openflexo.localization.FlexoLocalization;
 import org.openflexo.ve.VECst;
@@ -65,7 +69,7 @@ public class CreateDiagramInitializer extends ActionInitializer<CreateDiagram, V
 
 	private Status configureModelSlot(CreateDiagram action, ModelSlot configuredModelSlot) {
 		return instanciateShowDialogAndReturnStatus(action.getModelSlotInstanceConfiguration(configuredModelSlot),
-				VECst.CONFIGURE_MODEL_SLOT_INSTANCE_DIALOG_FIB);
+				getModelSlotInstanceConfigurationFIB(configuredModelSlot.getClass()));
 	}
 
 	@Override
@@ -140,6 +144,24 @@ public class CreateDiagramInitializer extends ActionInitializer<CreateDiagram, V
 	@Override
 	protected Icon getEnabledIcon() {
 		return VEIconLibrary.DIAGRAM_ICON;
+	}
+
+	/**
+	 * @author Vincent This method has to be removed as soon as we will have a real Wizard Management. Its purpose is to handle the
+	 *         separation of FIBs for Model Slot Configurations.
+	 * @return File that correspond to the FIB
+	 */
+	private File getModelSlotInstanceConfigurationFIB(Class modelSlotClass) {
+		if (TypeAwareModelSlot.class.isAssignableFrom(modelSlotClass)) {
+			return VECst.CONFIGURE_TYPESAFE_MODEL_SLOT_INSTANCE_DIALOG_FIB;
+		}
+		if (FreeModelSlot.class.isAssignableFrom(modelSlotClass)) {
+			return VECst.CONFIGURE_FREE_MODEL_SLOT_INSTANCE_DIALOG_FIB;
+		}
+		if (VirtualModelModelSlot.class.isAssignableFrom(modelSlotClass)) {
+			return VECst.CONFIGURE_FREE_MODEL_SLOT_INSTANCE_DIALOG_FIB;
+		}
+		return null;
 	}
 
 }
