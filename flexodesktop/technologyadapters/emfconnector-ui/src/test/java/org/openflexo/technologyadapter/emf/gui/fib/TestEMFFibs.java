5954a779b14f6e69b01ed8942d2697146350d54a
Sylvain Guérin <sylvain@openflexo.org>
2013-09-11 16:13:12 +0200

    MEDIUM / Add TechnologyAdapter resources to right project (ui) and setup JUnit tests on FIBs and inspectors

---
+++ flexodesktop/technologyadapters/emfconnector-ui/src/test/java/org/openflexo/technologyadapter/emf/gui/fib/TestEMFFibs.java
@@ -0,0 +1,11 @@
+package org.openflexo.technologyadapter.emf.gui.fib;
+
+import org.openflexo.fib.FIBTestCase;
+
+public class TestEMFFibs extends FIBTestCase {
+
+	/*public void testXXX() {
+		validateFIB("Fib/XXX.fib");
+	}*/
+
+}
