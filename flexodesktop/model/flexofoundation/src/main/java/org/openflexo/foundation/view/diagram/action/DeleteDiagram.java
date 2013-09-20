d5ed86d84e1bb1a4e1ca8733a2eb3f866ef305ef
Sylvain Guérin <sylvain@openflexo.org>
2013-09-20 13:02:44 +0200

    MEDIUM / Implements VirtualModelInstance/Diagram deletion

---
+++ flexodesktop/model/flexofoundation/src/main/java/org/openflexo/foundation/view/diagram/action/DeleteDiagram.java
@@ -0,0 +1,76 @@
+/*
+ * (c) Copyright 2010-2011 AgileBirds
+ *
+ * This file is part of OpenFlexo.
+ *
+ * OpenFlexo is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OpenFlexo is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OpenFlexo. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package org.openflexo.foundation.view.diagram.action;
+
+import java.util.Vector;
+import java.util.logging.Logger;
+
+import org.openflexo.foundation.FlexoEditor;
+import org.openflexo.foundation.FlexoModelObject;
+import org.openflexo.foundation.FlexoObject;
+import org.openflexo.foundation.action.FlexoAction;
+import org.openflexo.foundation.action.FlexoActionType;
+import org.openflexo.foundation.view.diagram.model.Diagram;
+
+public class DeleteDiagram extends FlexoAction<DeleteDiagram, Diagram, FlexoObject> {
+
+	private static final Logger logger = Logger.getLogger(DeleteDiagram.class.getPackage().getName());
+
+	public static FlexoActionType<DeleteDiagram, Diagram, FlexoObject> actionType = new FlexoActionType<DeleteDiagram, Diagram, FlexoObject>(
+			"delete_diagram", FlexoActionType.editGroup, FlexoActionType.DELETE_ACTION_TYPE) {
+
+		/**
+		 * Factory method
+		 */
+		@Override
+		public DeleteDiagram makeNewAction(Diagram focusedObject, Vector<FlexoObject> globalSelection, FlexoEditor editor) {
+			return new DeleteDiagram(focusedObject, globalSelection, editor);
+		}
+
+		@Override
+		public boolean isVisibleForSelection(Diagram diagram, Vector<FlexoObject> globalSelection) {
+			return true;
+		}
+
+		@Override
+		public boolean isEnabledForSelection(Diagram diagram, Vector<FlexoObject> globalSelection) {
+			return diagram != null;
+		}
+
+	};
+
+	static {
+		FlexoModelObject.addActionForClass(DeleteDiagram.actionType, Diagram.class);
+	}
+
+	DeleteDiagram(Diagram focusedObject, Vector<FlexoObject> globalSelection, FlexoEditor editor) {
+		super(actionType, focusedObject, globalSelection, editor);
+	}
+
+	@Override
+	protected void doAction(Object context) {
+		logger.info("Delete diagram");
+
+		if (getFocusedObject().getResource() != null) {
+			getFocusedObject().getResource().delete();
+		}
+	}
+
+}
