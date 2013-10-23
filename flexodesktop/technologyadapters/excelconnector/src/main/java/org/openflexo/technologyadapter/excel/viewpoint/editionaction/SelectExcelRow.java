1b95b18e8abf7a3e42baa45faf9412bfdde66f7d
Vincent <vincent@openflexo.org>
2013-10-23 22:01:36 +0200

    LOW / Update excel viewpoint, improve fetch rows.

--- flexodesktop/technologyadapters/excelconnector/src/main/java/org/openflexo/technologyadapter/excel/viewpoint/editionaction/SelectExcelRow.java
+++ flexodesktop/technologyadapters/excelconnector/src/main/java/org/openflexo/technologyadapter/excel/viewpoint/editionaction/SelectExcelRow.java
@@ -1,16 +1,19 @@
 package org.openflexo.technologyadapter.excel.viewpoint.editionaction;
 
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
 
+import org.openflexo.antar.binding.DataBinding;
+import org.openflexo.antar.expr.NullReferenceException;
+import org.openflexo.antar.expr.TypeMismatchException;
 import org.openflexo.foundation.view.action.EditionSchemeAction;
 import org.openflexo.foundation.viewpoint.FetchRequest;
 import org.openflexo.foundation.viewpoint.VirtualModel;
 import org.openflexo.foundation.viewpoint.annotations.FIBPanel;
 import org.openflexo.technologyadapter.excel.BasicExcelModelSlot;
-import org.openflexo.technologyadapter.excel.model.ExcelCell;
 import org.openflexo.technologyadapter.excel.model.ExcelRow;
 import org.openflexo.technologyadapter.excel.model.ExcelSheet;
 import org.openflexo.technologyadapter.excel.model.ExcelWorkbook;
@@ -20,6 +23,8 @@ public class SelectExcelRow extends FetchRequest<BasicExcelModelSlot, ExcelRow> 
 
 	private static final Logger logger = Logger.getLogger(SelectExcelRow.class.getPackage().getName());
 
+	private DataBinding<ExcelSheet> excelSheet;
+	
 	public SelectExcelRow(VirtualModel.VirtualModelBuilder builder) {
 		super(builder);
 		// TODO Auto-generated constructor stub
@@ -44,13 +49,50 @@ public class SelectExcelRow extends FetchRequest<BasicExcelModelSlot, ExcelRow> 
 
 		ExcelWorkbook excelWorkbook = (ExcelWorkbook) getModelSlotInstance(action).getResourceData();
 
-		List<ExcelRow> selectedExcelRows = new ArrayList<ExcelRow>(0);
-		for(ExcelSheet excelSheet : excelWorkbook.getExcelSheets()){
-			selectedExcelRows.addAll(excelSheet.getExcelRows());
+		List<ExcelRow> selectedExcelRows = new ArrayList<ExcelRow>();
+		ExcelSheet excelSheet;
+		try {
+			excelSheet = getExcelSheet().getBindingValue(action);
+		
+			if(excelSheet!=null){
+				selectedExcelRows.addAll(excelSheet.getExcelRows());
+			}
+			else{
+				for(ExcelSheet excelSheetItem : excelWorkbook.getExcelSheets()){
+					selectedExcelRows.addAll(excelSheetItem.getExcelRows());
+				}
+			}
+		} catch (TypeMismatchException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NullReferenceException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 
 		List<ExcelRow> returned = filterWithConditions(selectedExcelRows, action);
 
 		return returned;
+	}
+	
+	public DataBinding<ExcelSheet> getExcelSheet() {
+		if (excelSheet == null) {
+			excelSheet = new DataBinding<ExcelSheet>(this, ExcelSheet.class, DataBinding.BindingDefinitionType.GET);
+			excelSheet.setBindingName("excelSheet");
+		}
+		return excelSheet;
+	}
+
+	public void setExcelSheet(DataBinding<ExcelSheet> excelSheet) {
+		if (excelSheet != null) {
+			excelSheet.setOwner(this);
+			excelSheet.setDeclaredType(ExcelSheet.class);
+			excelSheet.setBindingDefinitionType(DataBinding.BindingDefinitionType.GET);
+			excelSheet.setBindingName("excelSheet");
+		}
+		this.excelSheet = excelSheet;
 	}
 }
