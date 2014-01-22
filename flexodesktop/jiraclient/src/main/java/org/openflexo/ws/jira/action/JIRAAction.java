f23ee6ed82d4706a5ef47e4d00444928cc4f45e0
Sylvain Guérin <sylvain@openflexo.org>
2014-01-22 13:00:47 +0100

    MEDIUM / Moved some stuff to Openflexo-Utils

--- flexodesktop/jiraclient/src/main/java/org/openflexo/ws/jira/action/JIRAAction.java
+++
@@ -1,8 +0,0 @@
-package org.openflexo.ws.jira.action;
-
-import org.openflexo.ws.jira.result.JIRAResult;
-
-public abstract class JIRAAction<R extends JIRAResult> {
-
-	public abstract Class<R> getResultClass();
-}
--- flexodesktop/jiraclient/src/main/java/org/openflexo/ws/jira/result/SubmitIssueResult.java
+++
@@ -1,5 +0,0 @@
-package org.openflexo.ws.jira.result;
-
-public class SubmitIssueResult extends JIRAResult {
-
-}
